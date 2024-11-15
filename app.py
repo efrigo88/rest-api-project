ffa22f186c22833ae2c0164daea631d8139dedca
Emiliano Frigo <emiliano@factory14.com>
2024-11-15 11:33:32 +0100

    add more endpoints and refactor

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ def get_stores():
     return {"stores": list(stores.values())}
 
 
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
@@ -31,6 +39,28 @@ def create_store():
     return store, 201
 
 
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
@@ -43,7 +73,7 @@ def create_item():
             400,
             message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
         )
-    for item in item.values():
+    for item in items.values():
         if (
             item_data["name"] == item["name"]
             and item_data["store_id"] == item["store_id"]
@@ -60,22 +90,26 @@ def create_item():
     return item, 201
 
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
     try:
-        return stores[store_id]
+        del items[item_id]
+        return {"message": "Item deleted."}
     except KeyError:
-        abort(404, message="Store not found.")
+        abort(404, message="Item not found.")
 
 
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.",
+        )
     try:
-        return items[item_id]
+        item = items[item_id]
+        item |= item_data
+        return item
     except KeyError:
         abort(404, message="Item not found.")
