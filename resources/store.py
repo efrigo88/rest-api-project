8f9d6ff198ee1d30a826066ee6283d3133e3fc01
Emiliano Frigo <emiliano@factory14.com>
2024-11-18 20:24:47 +0100

    add marshmallow schema validations

--- resources/store.py
+++ resources/store.py
@@ -5,6 +5,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from db import stores
+from schemas import StoreSchema
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
@@ -30,12 +31,8 @@ class StoreList(MethodView):
     def get(self):
         return {"stores": list(stores.values())}
 
-    def post(self):
-        store_data = request.get_json()
-
-        if "name" not in store_data:
-            abort(400, message="Ensure 'name' is included in the JSON payload.")
-
+    @blp.arguments(StoreSchema)
+    def post(self, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(400, message="Store already exists.")
