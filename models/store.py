4f6f505eaf5699a48b1089b6bade61b05f43425a
Emiliano Frigo <emiliano@factory14.com>
2024-11-21 17:44:49 +0100

    add tags

--- models/store.py
+++ models/store.py
@@ -12,3 +12,6 @@ class StoreModel(db.Model):
         lazy="dynamic",
         cascade="all, delete",
     )
+    tags = db.relationship(
+        "TagModel", back_populates="store", lazy="dynamic", cascade="all, delete"
+    )
