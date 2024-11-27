49d44585fd93ef3dd8f905ca3fcf0f68de19159f
Emiliano Frigo <emiliano@factory14.com>
2024-11-27 12:34:02 +0100

    add sleep to wait for a healthy postgres deploy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+until pg_isready -h db -U user; do
+  echo "Waiting for database to be ready..."
+  sleep 2
+done
 
 flask db upgrade
-
 exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
